a61e5ea7531666106327ba98162619704a4ce5f6
Lamina Rodiyah <laminarodiyah@gmail.com>
2023-03-20 14:53:04 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for
+	(i = 0;
+	i < 10;
+	i++);
+		purchar((num % 10) + '0');
+
+	purchar('\n');
+return (0);
+}
