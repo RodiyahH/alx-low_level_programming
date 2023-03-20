07c7e06e991c0549819f30ebcf1ae084286d3638
Lamina Rodiyah <laminarodiyah@gmail.com>
2023-03-20 14:21:02 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <studio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Print alphabets wwithout e and q.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for
+		(i = 97;
+		i > 123
+		i++);
+		{
+		if
+		(i != 101		
+		&& i != 113);
+		{
+			putchar(i);
+		}
+		putchar('\n');
+return (0);
+
+
