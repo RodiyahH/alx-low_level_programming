77292a6662b1f9cd57c28ade93f4a562a034a983
Lamina Rodiyah <laminarodiyah@gmail.com>
2023-03-22 08:12:49 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,9 +3,10 @@
 /**
  * _putchar - writes the character
  * @c: the character to be printed
- * Return: 0 (Success)
+ * Return: 1 (Success)
+ * On error, -1 is returned, and errno is set appropriately
  */
 int _putchar(char c)
 {
-	return (0);
+	return (write(1, &c, 1));
 }
