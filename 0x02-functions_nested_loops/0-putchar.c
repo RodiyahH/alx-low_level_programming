ece3ae20cca657824f3b342b046edab6c09be759
Lamina Rodiyah <laminarodiyah@gmail.com>
2023-03-22 08:37:00 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,18 @@
 #include "main.h"
-#include <unistd.h>
 /**
- * _putchar - writes the character
- * @c: the character to be printed
+ * main  - Prints _putchar
  * Return: 0 (Success)
  */
-int _putchar(char c)
+int _main(void)
 {
+	_putchar('_')
+	_putchar('p')
+	_putchar('u')	
+	_putchar('t')
+	_putchar('c')
+	_putchar('h')
+	_putchar('a')
+	_putchar('r')
+	_putchar('\n')	
 	return (0);
 }
