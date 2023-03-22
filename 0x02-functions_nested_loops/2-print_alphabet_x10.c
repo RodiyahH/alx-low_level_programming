e82ac958d646ba5b7e4f63d3b5369f11bcda0829
Lamina Rodiyah <laminarodiyah@gmail.com>
2023-03-22 12:23:38 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void);
+{
+	int i, co;
+
+	co = 0
+
+	while (co < 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		co++
+		_putchar('/n');
+	}
+}
