c7f95d23653444491da5120190a42b5e192cf513
Lamina Rodiyah <laminarodiyah@gmail.com>
2023-03-23 08:35:45 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
@@ -26,3 +27,4 @@ int largest_number(int a, int b, int c)
 
 	return (largest);
 }
+
