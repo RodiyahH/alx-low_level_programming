f1df5f686987273b1ccfe141c739f85bdb5952f7
Lamina Rodiyah <laminarodiyah@gmail.com>
2023-03-24 15:22:37 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,11 +3,12 @@
 /**
  * _isdigit -  a function that checks for a digit 0-9
  * @c: input
- * Return: 1 if c is a digit, 0 otherwise
+ * Return: 0 or 1
  */
+
 int _isdigit(int c)
 {
-	if ((c >= 48) && (c <= 57))
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
